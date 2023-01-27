429c7128098eadfcfb2775ba117236f4187fd524
Alucard <85690382+Alucard2169@users.noreply.github.com>
2023-01-27 15:13:50 +0530

    Delete client directory

--- client/src/assets.js
+++
@@ -1,8 +0,0 @@
-import hero0 from './style/hero/hero0.jpg'
-import hero1 from './style/hero/hero1.jpg'
-import hero2 from './style/hero/hero2.jpg'
-import hero3 from './style/hero/hero3.jpg'
-
-const heroBackground = [    hero0,hero1,hero2,hero3 ]
-
-export default heroBackground;
--- client/src/context/AppContext.jsx
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-
-const SearchTerm = createContext(null)
-
-
-export default SearchTerm;
