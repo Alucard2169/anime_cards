dee62fb88a646a8d3a92d798ea8ab8c6bdc534d7
Alucard2169 <ashish10deepanshu@gmail.com>
2023-04-06 15:57:20 +0530

    organized code

--- router/dataRouter.js
+++ router/dataRouter.js
@@ -1,18 +1,29 @@
-const express = require('express');
-const { getAnime, getAnimeById, filterAnime, animeByName, getCharacter } = require('../controller/dataController');
-const apicache = require('apicache');
+const express = require("express");
+const {
+  getAnime,
+  getAnimeById,
+  filterAnime,
+  animeByName,
+  getCharacter,
+} = require("../controller/dataController");
+const apicache = require("apicache");
 
 const router = express.Router();
-let cache = apicache.middleware
+let cache = apicache.middleware;
 
-router.get(`/api`, cache('2 minutes'), getAnime)
+// initial anime request
+router.get(`/api`, cache("2 minutes"), getAnime);
 
-router.get('/api/anime/:id', cache('2 minutes'), getAnimeById)
+// get anime based on id
+router.get("/api/anime/:id", cache("2 minutes"), getAnimeById);
 
-router.get(`/top/:filter`, cache('2 minutes'), filterAnime)
+//get anime based on user preference (filter (populatity,etc))
+router.get(`/top/:filter`, cache("2 minutes"), filterAnime);
 
-router.get('/anime/:name', animeByName)
+// get anime by name
+router.get("/anime/:name", animeByName);
 
-router.get('/api/characters/:id',cache('2 minutes'), getCharacter)
+// get anime characters
+router.get("/api/characters/:id", cache("2 minutes"), getCharacter);
 
 module.exports = router;
--- src/src/App.js
+++ src/src/App.js
@@ -1,31 +1,24 @@
-import Heropage from './homepage';
-import Navbar from './components/navbar';
-import {HashRouter as Router, Route, Routes } from 'react-router-dom';
-import AnimeList from './animeList';
-import AnimeDetails from './animeDetails';
-
-
+import Heropage from "./pages/homepage";
+import Navbar from "./components/navbar";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import AnimeList from "./pages/animeList";
+import AnimeDetails from "./pages/animeDetails";
 
 const App = () => {
-
-
-
   return (
     <Router>
-      
-         <div className="App">
-          <Navbar />
+      <div className="App">
+        <Navbar />
         <div className="content">
           <Routes>
-            <Route path="/" element={<Heropage/>}></Route>
+            <Route path="/" element={<Heropage />}></Route>
             <Route path="/anime" element={<AnimeList />}></Route>
-            <Route path="/anime/:id" element={<AnimeDetails/>}></Route>
+            <Route path="/anime/:id" element={<AnimeDetails />}></Route>
           </Routes>
         </div>
       </div>
-      
-   </Router>
+    </Router>
   );
-}
+};
 
 export default App;
