8fdbc3fd62a690a8e446fe74dd149e54962ed874
Alucard2169 <alucardd2169@gmail.com>
2023-07-28 13:55:13 +0530

    added a  link to new version

--- old_Version/client/src/components/navbar.js
+++ old_Version/client/src/components/navbar.js
@@ -1,10 +1,10 @@
+import { Link, useNavigate } from 'react-router-dom';
 import favicon from '../favicon.png';
 import '../style/navbar.css';
 import search_icon from '../style/search_icon.svg';
-import { Link, useNavigate} from 'react-router-dom';
 
 import { useContext } from 'react';
-import {SearchTerm} from '../context/appContext'
+import { SearchTerm } from '../context/appContext';
 
 const Navbar = () => {
     
@@ -31,20 +31,31 @@ const Navbar = () => {
         }
     }
 
-    return ( 
-        <>
+    return (
+      <>
         <nav className="navbar">
-            <div className='logo'><Link to="/"><img src={favicon} className="mainLogo"alt="hello" /></Link></div>
-            <div className="left">
-                <label>
-                    <img src={search_icon} alt="" />
-                        <input type="text" id="search_query" className="search" placeholder="search for an anime, e.g. Bleach" onKeyUp={nameSearch} onChange={handleChange} />
-                </label>
-            </div>
-            </nav>
-            
-        </>
-     );
+          <div className="logo">
+            <Link to="/">
+              <img src={favicon} className="mainLogo" alt="hello" />
+            </Link>
+          </div>
+              <a href="https://anime-cards-alpha.vercel.app/" target='_blank' className='newVer'>New Version</a>
+          <div className="left">
+            <label>
+              <img src={search_icon} alt="" />
+              <input
+                type="text"
+                id="search_query"
+                className="search"
+                placeholder="search for an anime, e.g. Bleach"
+                onKeyUp={nameSearch}
+                onChange={handleChange}
+              />
+            </label>
+          </div>
+        </nav>
+      </>
+    );
 }
  
 export default Navbar;
